97f5098759a25709bbb0be4083663b183764d566
Vordgi <sasha2822222@gmail.com>
2021-12-26 12:26:38 +0200

    T-7 install and configure jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ["src/lib/**/*.{js,ts}", "!src/lib/loadColors.ts"],
+  coverageReporters: ["text-summary"],
+  testRegex: "src/tests/.*\\.test\\.ts$",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
